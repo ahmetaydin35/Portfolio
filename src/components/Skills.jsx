9c7fb1a78b60b437f7fdc001db3de142e07a35e0
Ahmet Aydin <ahm3t35@gmail.com>
2024-04-15 13:42:39 +0300

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <section className="py-20 align-element " id="skills">
       <SectionTitle text="tech stack" />
-      <div className="py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-5">
+      <div className="py-16 grid gap-8 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5">
         {skills.map((skill) => {
           return <SkillsCard key={skill.id} {...skill} />;
         })}
